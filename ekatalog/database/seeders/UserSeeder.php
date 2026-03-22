18327bc089e168d8ad61eff679d63ad197246623
Milos Bobic <bobicm02@gmail.com>
2026-03-22 11:00:50 +0100

    messed up seeders and fixed them

---
+++ ekatalog/database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name'     => 'Admin',
+            'email'    => 'admin@ekatalog.com',
+            'password' => Hash::make('admin123'),
+            'is_admin' => true,
+        ]);
+
+        User::create([
+            'name'     => 'User',
+            'email'    => 'user@ekatalog.com',
+            'password' => Hash::make('user123'),
+            'is_admin' => false,
+        ]);
+
+    }
+}
